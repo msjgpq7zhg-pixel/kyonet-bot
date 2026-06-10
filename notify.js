42ed021973a3c8a5dab97eecdd1b9ace5a7a1e59
msjgpq7zhg-pixel <msjgpq7zhg@privaterelay.appleid.com>
2026-06-11 08:16:21 +0900

    Update notify.js

--- notify.js
+++ notify.js
@@ -101,10 +101,8 @@ console.log("=====通知用=====");
 const today = new Date();
 today.setHours(0,0,0,0);
 
-const deadlineDate = new Date(deadline);
-deadlineDate.setHours(0,0,0,0);
-
 for (const item of targets) {
+
     const deadline = item.match(/期限：(\d{4}\/\d{2}\/\d{2})/)?.[1] || "不明";
 
     const title = item
@@ -115,16 +113,16 @@ for (const item of targets) {
         .trim();
 
     const deadlineDate = new Date(deadline);
+    deadlineDate.setHours(0,0,0,0);
 
-    const diffDays = Math.ceil(
-        (deadlineDate - today) / (1000 * 60 * 60 * 24)
-    );
+    const diffDays =
+        (deadlineDate - today) / (1000 * 60 * 60 * 24);
 
-   const type = item.startsWith("テスト")
-    ? "テスト📝"
-    : "課題📄";
+    const type = item.startsWith("テスト")
+        ? "テスト📝"
+        : "課題📄";
 
-console.log(`${type} ${title}`);
+    console.log(`${type} ${title}`);
     console.log(`【期限】 ${deadline}（あと${diffDays}日）`);
     console.log("");
 }
