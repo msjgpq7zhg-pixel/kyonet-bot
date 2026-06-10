0e3f5df3fa8c3bdb33f557f38408330baa763360
msjgpq7zhg-pixel <msjgpq7zhg@privaterelay.appleid.com>
2026-06-10 10:56:17 +0900

    Integrate environment variables for login credentials
    Added environment variables for credentials and updated log messages.

--- notify.js
+++ notify.js
@@ -1,5 +1,8 @@
 const puppeteer = require("puppeteer");
 
+const ID = process.env.KYONET_ID;
+const PASS = process.env.KYONET_PASS;
+
 (async () => {
 
     const browser = await puppeteer.launch({
@@ -13,7 +16,17 @@ const puppeteer = require("puppeteer");
         waitUntil: "networkidle2"
     });
 
-    console.log("ページ開けた");
+    console.log("ログインページ開いた");
+
+    // ↓ここは後で調整する可能性あり
+    await page.type("input[type='text']", ID);
+    await page.type("input[type='password']", PASS);
+
+    await page.click("button[type='submit']");
+
+    await page.waitForNavigation();
+
+    console.log("ログイン成功");
 
     await browser.close();
 
